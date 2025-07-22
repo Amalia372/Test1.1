97e848797c6fdf2cc2cc5a68be9bafac5932eab0
Amalia372 <amalia.dijmarescu@gmail.com>
2025-07-22 23:35:07 +0300

    Add files via upload

---
+++ test1.2.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://automationexercise.com/test_cases');
+  await page.getByRole('button', { name: 'Consent' }).click();
+  await page.getByRole('link', { name: ' Signup / Login' }).click();
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').click();
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('amalia.dijmarescu@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('12345678');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: ' Delete Account' }).click();
+  await page.getByRole('link', { name: 'Continue' }).click();
+});
